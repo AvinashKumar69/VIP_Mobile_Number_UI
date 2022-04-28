f3619923480c795f3965989418e6510192815fe7
Avinash Kumar <ak2449746@gmail.com>
2022-04-28 07:29:01 +0530

    Update...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,7 +52,7 @@ const Footer = () => {
                                 <input type="text" id="footer-field" name="footer-field" placeholder='abc@example.com' className="w-full bg-gray-200 rounded-lg text-base text-white py-1 px-3" />
                             </div>
                             <button className="text-white py-2 px-6 rounded-full sm:mx-auto sm:mt-1 xs:mx-auto xs:mt-1"
-                                style={{ backgroundColor: 'rgb(242, 56, 0)' }}
+                                style={{ backgroundColor: 'rgb(247 121 1)' }}
                             >
                                 Subscribe
                             </button>
@@ -92,6 +92,12 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
+
+            <a className='chat-icon-fixed' href='/' target='_blank' rel='noreferrer'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-chat h-8 w-8 mx-auto" viewBox="0 0 16 16">
+                    <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                </svg>
+            </a>
         </>
     )
 }
